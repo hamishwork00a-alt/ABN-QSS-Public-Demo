058fbe38c97150a108ddb9b300a094caea0df86e
hamishwork00a-alt <hamishwork00a@gmail.com>
2025-11-04 17:25:57 +0800

    Initialize ABN-QSS Public Demo Package

---
+++ public-demo/abn_qss_demo/__init__.py
@@ -0,0 +1,15 @@
+"""
+ABN-QSS Public Demo Package
+量子增强科研计算平台 - 公开演示版
+"""
+
+__version__ = "0.1.0"
+__author__ = "ABN-QSS Team"
+
+from .safe_core import QuantumResearchPlatform, MaterialScienceTools, PharmaResearchTools
+
+__all__ = [
+    "QuantumResearchPlatform",
+    "MaterialScienceTools", 
+    "PharmaResearchTools"
+]
