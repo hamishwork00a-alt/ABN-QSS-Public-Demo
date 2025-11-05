92504c41c3b3047f87de5884792233fdc86d8e39
hamishwork00a-alt <hamishwork00a@gmail.com>
2025-11-05 11:31:04 +0800

    Update __init__.py

--- public-demo/abn_qss_demo/__init__.py
+++ public-demo/abn_qss_demo/__init__.py
@@ -1,15 +1,13 @@
-"""
-ABN-QSS Public Demo Package
-量子增强科研计算平台 - 公开演示版
-"""
-
-__version__ = "0.1.0"
-__author__ = "ABN-QSS Team"
-
 from .safe_core import QuantumResearchPlatform, MaterialScienceTools, PharmaResearchTools
+from .health_monitoring import HealthMonitoringSystem, MetabolicMirror
 
 __all__ = [
     "QuantumResearchPlatform",
     "MaterialScienceTools", 
-    "PharmaResearchTools"
+    "PharmaResearchTools",
+    "HealthMonitoringSystem",
+    "MetabolicMirror"
 ]
+
+__version__ = "0.1.0"
+__author__ = "ABN-QSS Team"
